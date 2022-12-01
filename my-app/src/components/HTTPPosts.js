6748bd05114a9d0802194b7ad15820210e6d62b4
DOdrums <dirkornee@hotmail.com>
2022-12-01 13:45:49 +0000

    Fix error in HTTPPosts component

--- my-app/src/components/HTTPPosts.js
+++ my-app/src/components/HTTPPosts.js
@@ -1,53 +1,49 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-export class HTTPPosts extends Component {
+export class HTTPPost extends Component {
     constructor(props) {
-      super(props)
+        super(props)
     
-      this.state = {
-         apiResponse: null
-      }
-    }
-
-    postToApi() {
-        axios.post('https://jsonplaceholder.typicode.com/posts',
-        {
-            title: "Hello world!",
-            body: 'It works!',
-            userId: 123,
+        this.state = {
+             apiResponse: null
         }
+    }
+    
+    postToApi = () => {
+        axios.post(
+            'https://jsonplaceholder.typicode.com/posts',
+            {
+                title: 'Hello world!',
+                body: 'It works!',
+                userId: 123,
+            }
         ).then(response => {
-            console.log(response)
             this.setState({
                 apiResponse: response.data
             })
         })
     }
-
     render() {
-        const {apiResponse} = this.state
+        const {apiResponse} = this.state;
         return (
-        <div>
-            <button onClick={this.postToApi}>
-                Create Post
-            </button>
+            <div>
+                <button onClick={this.postToApi}>
+                    Create Post
+                </button>
                 {
                     apiResponse
-                    ? (
-                        <div>
-                            <h1>{apiResponse.title}</h1>
-                            <p>post id: {apiResponse.id}</p>
-                            <p>{apiResponse.body}</p>
-                            <p>used id: {apiResponse.userId}</p>
-                        </div>
-                    ) : (
-                        <p>please click the button above</p>
-                    )
+                    ? (<div>
+                        <h1>{apiResponse.title}</h1>
+                        <p>post id: {apiResponse.id}</p>
+                        <p>{apiResponse.body}</p>
+                        <p>user id: {apiResponse.userId}</p>
+                    </div>)
+                    : (<p>please click the button above</p>)
                 }
-        </div>
+            </div>
         )
     }
 }
 
-export default HTTPPosts
+export default HTTPPost
